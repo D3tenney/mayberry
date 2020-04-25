969776211965ded936d6b90dcdf9f9e1140a5267
Devon <dtenney@pop-os.localdomain>
2020-04-24 22:49:33 -0400

    slight modification to config script to create flask_sqlalchemy vars

--- app/config.py
+++ app/config.py
@@ -4,15 +4,6 @@ from os import environ
 
 load_dotenv(override=True)
 
-PG_USER = environ.get("PG_USER")
-PG_PASS = environ.get("PG_PASS")
-PG_HOST = environ.get("PG_HOST")
-PG_PORT = environ.get("PG_PORT")
-PG_DB = environ.get("PG_DB")
-
-engine_string = f"postgresql://{PG_USER}:{PG_PASS}@{PG_HOST}:{PG_PORT}/{PG_DB}"
-engine = create_engine(engine_string)
-
 
 class Config(object):
     '''
@@ -20,3 +11,15 @@ class Config(object):
     '''
     DEBUG = True
     TESTING = False
+
+    PG_USER = environ.get("PG_USER")
+    PG_PASS = environ.get("PG_PASS")
+    PG_HOST = environ.get("PG_HOST")
+    PG_PORT = environ.get("PG_PORT")
+    PG_DB = environ.get("PG_DB")
+
+    PG_CXN = f"postgresql://{PG_USER}:{PG_PASS}@{PG_HOST}:{PG_PORT}/{PG_DB}"
+    engine = create_engine(PG_CXN)
+
+    SQLALCHEMY_DATABASE_URI = PG_CXN
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
